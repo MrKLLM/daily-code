6a9be2a2b335c97a6a0750dd660e443dbec565b6
MrKLLM <3553999325@qq.com>
2026-09-11 21:43:16 +0800

    补充 ExpressionTree 的 ExpressionMapper

--- C#/Temp/ExpressionTree/Program.cs
+++ C#/Temp/ExpressionTree/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace ExpressionTree
+using System.Linq.Expressions;
+
+namespace ExpressionTree
 {
-    public class ExpressionMapper
+    public class ExpressionMapper<TIn,TOut>
     {
-        
+        public static Func<TIn, TOut>? _Func = null;
+
+        static ExpressionMapper(){
+            ParameterExpression parameterExp = Expression.Parameter(typeof(TIn), "p");//定义一个参数表达式，表示输入参数
+            List<MemberBinding> memberBinding = new List<MemberBinding>();
+            //绑定属性
+            foreach (var item in typeof(TOut).GetProperties())
+            {
+                MemberExpression member = Expression.Property(parameterExp, typeof(TIn).GetProperty(item.Name)!);
+
+            }
+        }
     }
 }
