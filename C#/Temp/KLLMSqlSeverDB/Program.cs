d4a6495c6698d0f9a613bd8603804718ee093c09
MrKLLM <3553999325@qq.com>
2026-09-14 21:08:52 +0800

    新增 KLLMSqlSeverDB 项目，包含数据库连接和查询功能，添加相关类和配置文件

---
+++ C#/Temp/KLLMSqlSeverDB/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Data.SqlClient;
+
+namespace KLLMSqlSeverDB
+{
+    class Program
+    {
+        //链接字符串--链接数据库用
+        public static readonly string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StuManeger;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+        static void Main(string[] args)
+        {
+            //定义sql语句
+            string sql = "select ClassName from StuClass where ClassName=N'农楚楚班'";
+            //链接sql语句
+            using (SqlConnection conn = new SqlConnection(connString)) //连接数据库，确保自动释放
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn) { CommandTimeout = 30 })
+                {
+                    conn.Open();//打开链接
+                    object res = cmd.ExecuteScalar();
+
+                    //展示数据
+                    Console.WriteLine("班级名称：" + (res.ToString()));
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ C#/Temp/KLLMSqlSeverDB/StuClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KLLMSqlSeverDB
+{
+    //班级实体类--在数据库中表示一行数据
+    public class StuClass
+    {
+        public int ClassId { get; set; } //班级编号
+        public string ClassName { get; set; }//班级名称
+    }
+}
